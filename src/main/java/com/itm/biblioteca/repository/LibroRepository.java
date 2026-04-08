b46c71409e5dfce8bd7dec851cba51a8a240394e
Victoria0127-git <mariacano306296@correo.itm.edu.co>
2026-04-08 16:08:37 -0500

    Agregado repositorio y controlador de Libros

---
+++ src/main/java/com/itm/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,11 @@
+
+package com.itm.biblioteca.repository;
+
+import com.itm.biblioteca.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+
+}
