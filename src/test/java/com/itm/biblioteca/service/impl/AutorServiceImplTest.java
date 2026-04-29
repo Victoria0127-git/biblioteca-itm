bfabfeb04c90415e2f80370cb79f95298b7de283
davidcampos311554-coder <davidcampos311554@correo.itm.edu.co>
2026-04-28 20:55:25 -0500

    Creacion y ejecucion de ambiente de pruebas Autor

--- src/test/java/com/itm/biblioteca/service/impl/AutorServiceImplTest.java
+++ src/test/java/com/itm/biblioteca/service/impl/AutorServiceImplTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -32,8 +34,39 @@ public class AutorServiceImplTest {
     @Test
     void crearAutor_RetornaAutorGuardado() {
         //Crear autor de prueba
-        Autor autorEntrada = new Autor();
+        Autor miAutor = Autor.builder()
+                .idAutor("A001")
+                .nombre("Gabriel")
+                .apellido("Garcia")
+                .nacionalidad("colombiana")
+                .build();
+        when(autorRepository.existsById("A001")).thenReturn(false); //Si pregunta la existenicia, que devuelva false
+        when(autorRepository.save(any(Autor.class))).thenReturn(miAutor); //Si guarda, que muestre el miAutor que creamos arriba
 
+        //WHEN
+        Autor resultado = autorService.crear(miAutor);
 
+        //THEN
+        assertNotNull(resultado); //Asegurar que el resultado no sea nulo
+        assertEquals(resultado.getIdAutor(), miAutor.getIdAutor()); //Asegurar que guarde correctamente
+        verify(autorRepository, times(1)).save(any(Autor.class)); //Comprobar que el servicio llamó el repositorio una vez
+
+    }
+
+    @Test
+    void crearAutor_ConExcepcionPorCampoVacio() {
+        //Creación de autor de prueba
+        Autor autorInvalido = Autor.builder()
+                .idAutor("A002")
+                .nombre("")
+                .apellido("Cervantes")
+                .nacionalidad("española")
+                .build();
+
+        //Fallar antes de preguntar al repositorio
+        RuntimeException ex =  assertThrows(RuntimeException.class, () -> autorService.crear(autorInvalido));
+        assertEquals("El nombre del autor es obligatorio.", ex.getMessage());
+
+        verify(autorRepository, never()).save(any()); //Verficar que no se guardó
     }
 }
