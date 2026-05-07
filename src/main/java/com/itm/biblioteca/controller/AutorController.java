20426de441db4e6b018f44a6a9deeeb2750a9337
davidcampos311554-coder <davidcampos311554@correo.itm.edu.co>
2026-05-07 18:22:00 -0500

    Eliminacion de librerías innecesarias (se pusieron al hacer pruebas)

--- src/main/java/com/itm/biblioteca/controller/AutorController.java
+++ src/main/java/com/itm/biblioteca/controller/AutorController.java
@@ -3,15 +3,11 @@ package com.itm.biblioteca.controller;
 import com.itm.biblioteca.model.Autor;
 import com.itm.biblioteca.service.IAutorService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
