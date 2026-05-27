887cecac5e6bd4d6215ab725baa9552613608135
davidcampos311554-coder <davidcampos311554@correo.itm.edu.co>
2026-05-27 18:30:31 -0500

    No prestar ejemplares que ya fueron prestados

--- src/main/java/com/itm/biblioteca/service/impl/PrestamoServiceImpl.java
+++ src/main/java/com/itm/biblioteca/service/impl/PrestamoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itm.biblioteca.service.impl;
 
+import com.itm.biblioteca.model.Ejemplar;
 import com.itm.biblioteca.model.Prestamo;
+import com.itm.biblioteca.repository.EjemplarRepository;
 import com.itm.biblioteca.repository.PrestamoRepository;
 import com.itm.biblioteca.service.IPrestamoService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PrestamoServiceImpl implements IPrestamoService {
     private final PrestamoRepository prestamoRepository;
+    private final EjemplarRepository ejemplarRepository;
 
     @Override
     public List<Prestamo> listarTodos() {
@@ -34,14 +37,19 @@ public class PrestamoServiceImpl implements IPrestamoService {
             throw new RuntimeException("Error: Un préstamo debe tener un miembro, un ejemplar y un bibliotecario asignado.");
         }
 
-        if (prestamoRepository.existsById(prestamo.getIdPrestamo()))
-        {
-            throw new RuntimeException("Ya existe prestamo con id "+prestamo.getIdPrestamo());
+        // 2. Verificación: Fechas lógicas (Permite mismo día)
+        if (prestamo.getFechaDevolucion() != null && prestamo.getFechaPrestamo() != null) {
+            if (prestamo.getFechaDevolucion().isBefore(prestamo.getFechaPrestamo())) {
+                throw new RuntimeException("La fecha de devolución no puede ser anterior a la fecha de préstamo.");
+            }
         }
 
-        // 2. Verificación: Fechas lógicas
-        if (prestamo.getFechaDevolucion().isBefore(prestamo.getFechaPrestamo())) {
-            throw new RuntimeException("La fecha de devolución no puede ser anterior a la fecha de préstamo.");
+        //Validar que el ejemplar no esté prestado ya.Primero buscamos el ejemplar
+        Ejemplar ejemplarReal = ejemplarRepository.findById(prestamo.getEjemplar().getId())
+                .orElseThrow(() -> new RuntimeException("Error: El ejemplar especificado no existe."));
+        // Si el estado es false, significa que ya está prestado
+        if (!ejemplarReal.getEstado()) {
+            throw new RuntimeException("Error: El ejemplar '" + ejemplarReal.getId() + "' ya está prestado actualmente.");
         }
 
         //Creamos el id personalizado automáticamente
@@ -49,11 +57,14 @@ public class PrestamoServiceImpl implements IPrestamoService {
         int siguienteNum = (maxNum == null) ? 1 : maxNum + 1;
         String nuevoIdPrestamo = String.format("P%03d", siguienteNum);
 
+        ejemplarReal.setEstado(false); //Prestamos el ejemplar
+        ejemplarRepository.save(ejemplarReal);
+
         //Construimos la base del prestamo con valores predeterminados
         Prestamo prestamoNuevo = Prestamo.builder()
                 .idPrestamo(nuevoIdPrestamo)
                 .fechaPrestamo(LocalDate.now())
-                .ejemplar(prestamo.getEjemplar())
+                .ejemplar(ejemplarReal)
                 .miembro(prestamo.getMiembro())
                 .bibliotecario(prestamo.getBibliotecario())
                 .build();
@@ -62,27 +73,36 @@ public class PrestamoServiceImpl implements IPrestamoService {
     }
 
     @Override
-    public Prestamo actualizar(String id, Prestamo prestamoActual){
-        return prestamoRepository.findById(id).
-                map(prestamoExistente -> {
-                    if ((prestamoActual.getFechaPrestamo() != null)) {
+    @Transactional
+    public Prestamo actualizar(String id, Prestamo prestamoActual) {
+        return prestamoRepository.findById(id)
+                .map(prestamoExistente -> {
+                    if (prestamoActual.getFechaPrestamo() != null) {
                         prestamoExistente.setFechaPrestamo(prestamoActual.getFechaPrestamo());
                     }
-                    if ((prestamoActual.getFechaDevolucion() != null)) {
+
+                    if (prestamoActual.getFechaDevolucion() != null) {
                         prestamoExistente.setFechaDevolucion(prestamoActual.getFechaDevolucion());
+
+                        // Cambiamos su bit a true (1) para que quede Disponible nuevamente
+                        if (prestamoExistente.getEjemplar() != null) {
+                            prestamoExistente.getEjemplar().setEstado(true);
+                        }
                     }
-                    if ((prestamoActual.getMiembro() != null)) {
+
+                    if (prestamoActual.getMiembro() != null) {
                         prestamoExistente.setMiembro(prestamoActual.getMiembro());
                     }
-                    if ((prestamoActual.getBibliotecario() != null)) {
+                    if (prestamoActual.getBibliotecario() != null) {
                         prestamoExistente.setBibliotecario(prestamoActual.getBibliotecario());
                     }
-                    if ((prestamoActual.getEjemplar() != null)) {
+                    if (prestamoActual.getEjemplar() != null) {
                         prestamoExistente.setEjemplar(prestamoActual.getEjemplar());
                     }
+
                     return prestamoRepository.save(prestamoExistente);
                 })
-                .orElseThrow(()-> new RuntimeException("El prestamo con el ID "+id+" no existe"));
+                .orElseThrow(() -> new RuntimeException("El prestamo con el ID " + id + " no existe"));
     }
 
     @Override
