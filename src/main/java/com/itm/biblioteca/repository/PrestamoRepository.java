0b9a8cd08a662561d2f14294b48998b2c6116a35
davidcampos311554-coder <davidcampos311554@correo.itm.edu.co>
2026-05-29 13:31:54 -0500

    Funciones evitar eliminar libros y miembros con prestamos

--- src/main/java/com/itm/biblioteca/repository/PrestamoRepository.java
+++ src/main/java/com/itm/biblioteca/repository/PrestamoRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PrestamoRepository extends JpaRepository<Prestamo, String> {
     @Query(value = "SELECT MAX(REPLACE(ID_Prestamo, 'P', '')) FROM Prestamo", nativeQuery = true)
     Integer findMaxIdNumeric();
+
+    boolean existsByMiembro_IdMiembroAndFechaDevolucionIsNull(String idMiembro);
+
+    boolean existsByEjemplar_Libro_IsbnAndFechaDevolucionIsNull(String isbn);
 }
